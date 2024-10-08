895c17c34caa4faa933ebfc3d6a6a6802643a7bf
ATSadi <ahnafsoad@gmail.com>
2024-10-09 00:46:52 +0600

    i

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <TonConnectUIProvider manifestUrl="https://raw.githubusercontent.com/ATSadi/WoolfsConnect/refs/heads/main/manifest2.json?token=GHSAT0AAAAAACWHMXG32LKBEJ56ZHN6YBQQZYFPPOA"
+    <TonConnectUIProvider manifestUrl="https://raw.githubusercontent.com/ATSadi/WoolfsConnect/refs/heads/main/manifest2.json"
     >
       <App />
     </TonConnectUIProvider>
